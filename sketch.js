69f8979d618fdd7876474bdf9b66ea859219d50f
Diego Herrera <64808209+dotDynamo@users.noreply.github.com>
2021-01-08 09:45:41 -0600

    Actualización Juego
    - Corregido color
    - Agregado límite de generaciones

--- sketch.js
+++ sketch.js
@@ -1,44 +1,61 @@
-let cont = 0,born = 0, die = 0;
+let cont = 0,born = 0, die = 0,alive = 0;
 let board;
 let columns;
 let rows;
 let cell_size = 10;
+let r,g,b;
+let c1 = 'white';
+let input, button;
+let lim;
 
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(600, 530);
+  input = createInput();
+  input.position(20, 498);
+  button = createButton('Generaciones');
+  button.position(180, 498);
+  button.mousePressed(limit);
   textSize(45);
   columns = width/cell_size;
-  rows = (height-100)/cell_size;
+  rows = (height-130)/cell_size;
   board = createBoard(columns, rows);
   for(let x = 1; x < columns-1; x++){
     for(let y = 1; y < rows-1; y++){
       board[x][y] = floor(random(2));
     }
   }
+  colorBoard(color('white'));
+  cont++;
+  let res = []
+  res = next();
+  born = 0; die = 0; alive = data = res[2];
+  genText(cont, born, die, alive,' ');
 }
 
 function draw() {
   background(0);
-  colorBoard();
-  if (keyIsPressed === true){
-    if (keyCode === RIGHT_ARROW)
+  let lim = input.value();
+  if (keyIsPressed === true && cont<lim){
+    if (keyCode === RIGHT_ARROW){
       cont++;
       let res = []
       res = next();
-      born = res[0]; die = res[1];
+      born = res[0]; die = res[1]; alive = data = res[2]; c1 = res[3];
+    }
   }
-  genText(cont, born, die);
+  genText(cont, born, die, alive,lim);
+  colorBoard(c1);
 }
   
 
-function colorBoard(){
+function colorBoard(c1){
   for(let x = 0; x < columns; x++){
     for(let y = 0; y < rows; y++){
       let posx = x*cell_size;
       let posy = y*cell_size;
       if(board[x][y] == 1){
-        fill(255);
         stroke(0);
+        fill(c1);
         rect(posx,posy,cell_size);
       }
     }
@@ -46,11 +63,8 @@ function colorBoard(){
 }
 
 function next(){
-  let r = random(255);
-  let g = random(255); 
-  let b = random(255); 
-  let a = random(255);
-  let data = [0,0];
+  r = floor(random(256)); g = floor(random(256)); b = floor(random(256));
+  let data = [0,0,0,color(r,g,b)];
   let next_board = createBoard(columns, rows);
   for(let x = 1; x < columns-1; x++){
     for(let y = 1; y < rows-1; y++){
@@ -59,33 +73,13 @@ function next(){
       neighbors = countNeighbors(x,y);
       if(cell == 0 && neighbors == 3){
         next_board[x][y] = 1;
-        let posx=x*cell_size;
-        let posy=y*cell_size;
-        if(board[x][y]==1){
-          fill(r, g, b, a);
-          stroke(0);
-          rect(posx,posy,cell_size,cell_size);
-       }
+        data[2]++;
         data[0]++;
       }else if(cell == 1 && (neighbors > 3 || neighbors < 2)){
         next_board[x][y] = 0;
-        let posx=x*cell_size;
-        let posy=y*cell_size;
-        if(board[x][y]==1){
-          fill(r, g, b, a);
-          stroke(0);
-          rect(posx,posy,cell_size,cell_size);
-       }
         data[1]++;
       }else{
         next_board[x][y] = cell;
-        let posx=x*cell_size;
-        let posy=y*cell_size;
-        if(board[x][y]==1){
-          fill(r, g, b, a);
-          stroke(0);
-          rect(posx,posy,cell_size,cell_size);
-       }
       }
     }
   }
@@ -114,12 +108,21 @@ function createBoard(cols,rws){
   return board;
 }
 
-function genText(cont, born, die){
+function genText(cont, born, die, alive, lim){
+  fill(100);
+  text(lim,20,440);
   fill(255,255,255);
   text(cont,20,440);
   text("GEN",20,490);
-  text(born,250,440);
-  text("BORN",250,490);
+  text(alive,160,440);
+  text("ALIVE",160,490);
+  text(born,320,440);
+  text("BORN",320,490);
   text(die,500,440);
   text("DIE",500,490);
+}
+
+function limit(){
+  button.attribute('disabled', '');
+  input.attribute('disabled','');
 }
